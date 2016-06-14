a333cb701699f574951c5470e9e460a86251549f
WangChun <wangchun@pindou.com>
2016-06-14 13:58:31 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ watch = require('gulp-watch'),
 
 var config = {
     base: './', // 当前文件所在目录
-    src: './resources/', // 所有开发资源目录
+    src: './src/', // 所有开发资源目录
     dist: './dist/', // 开发环境生成的静态资源--dev版本
     html: './views/'
 };
@@ -28,16 +28,19 @@ gulp.task('concat:js', ['libjs:copy'], function() {
             paths: {
                 Vue: 'lib/vue/dist/vue',
                 VueRouter: 'lib/vue-router/dist/vue-router',
+                VueAnimatedList: 'lib/vue-animated-list/vue-animated-list',
                 Utile:'js/utile/utile',
                 jquery:'lib/jquery.min',
                 App: 'js/main/main',
-                Index: 'js/index/index',
+                IndexHead: 'js/index/indexHead',
+                IndexList: 'js/index/indexList',
+                IndexPage: 'js/index/indexPage',
                 indexHeadHtml: 'html/index/indexHead.html',
                 indexPageHtml: 'html/index/indexPage.html',
                 indexListHtml: 'html/index/indexList.html'
             }
         }))
-        .pipe(concat('lib.js'))
+        .pipe(concat('index.js'))
         .pipe(gulp.dest(config.dist + 'js/'));
 });
 
@@ -89,7 +92,7 @@ gulp.task('libjs:copy', function() {
             config.src + 'lib/text/text.js',
             config.src + 'lib/require-css/css.js',
             config.src + 'lib/jquery.min.js',
-            config.src + 'js/require.config.js'
+            config.src + 'js/require.config.js',
         ])
         .pipe(gulp.dest(config.dist + 'js/'));
 });
@@ -111,7 +114,7 @@ gulp.task('watch', function() {
 
 gulp.task('connect', function() {
     connect.server({
-        root: ['views', './', 'resources'],
+        root: [config.base,config.html,config.dist],
         port: 8080,
         host: 'localhost',
         livereload: true
